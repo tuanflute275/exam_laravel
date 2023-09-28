066f0c138186db876ac51d4f42aab89f77f94889
Nguyễn Văn Tuấn <122516850+tuanflute275@users.noreply.github.com>
2023-09-28 23:08:54 +0700

    Update products.php

--- app/Models/products.php
+++ app/Models/products.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\categories;
 
 class products extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'image', 'price', 'discount', 'category_id'];
+
+    public function category(){
+        return $this->hasOne(categories::class, 'id', 'category_id');
+    }
 }
